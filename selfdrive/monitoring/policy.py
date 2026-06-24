237359810effdbbae49f988ddf4340d4ef058087
AndyDnn <44451242+AndyDnn@users.noreply.github.com>
2026-06-23 22:46:15 -0700

    Update policy.py

--- selfdrive/monitoring/policy.py
+++ selfdrive/monitoring/policy.py
@@ -25,19 +25,19 @@ def to_percent(v):
 class DRIVER_MONITOR_SETTINGS:
   def __init__(self):
     # https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:42018X1947&rid=2
-    self._WHEELTOUCH_POLICY_ALERT_1_TIMEOUT = 15.
-    self._WHEELTOUCH_POLICY_ALERT_2_TIMEOUT = 24.
-    self._WHEELTOUCH_POLICY_ALERT_3_TIMEOUT = 30.
+    self._WHEELTOUCH_POLICY_ALERT_1_TIMEOUT = 123456789
+    self._WHEELTOUCH_POLICY_ALERT_2_TIMEOUT = 123456789
+    self._WHEELTOUCH_POLICY_ALERT_3_TIMEOUT = 123456789
     # https://cdn.euroncap.com/cars/assets/euro_ncap_protocol_safe_driving_driver_engagement_v11_a30e874152.pdf
-    self._VISION_POLICY_ALERT_1_TIMEOUT = 3.
-    self._VISION_POLICY_ALERT_2_TIMEOUT = 5.
-    self._VISION_POLICY_ALERT_3_TIMEOUT = 11.
+    self._VISION_POLICY_ALERT_1_TIMEOUT = 123456789
+    self._VISION_POLICY_ALERT_2_TIMEOUT = 123456789
+    self._VISION_POLICY_ALERT_3_TIMEOUT = 123456789
 
     self._TIMEOUT_RECOVERY_FACTOR_MAX = 5.
     self._TIMEOUT_RECOVERY_FACTOR_MIN = 1.25
 
-    self._MAX_TERMINAL_ALERTS = 3  # not allowed to engage after 3 terminal alerts
-    self._MAX_TERMINAL_DURATION = int(30 / DT_DMON)  # not allowed to engage after 30s of terminal alerts
+    self._MAX_TERMINAL_ALERTS = 123456789  # not allowed to engage after 3 terminal alerts
+    self._MAX_TERMINAL_DURATION = int(300000 / DT_DMON)  # not allowed to engage after 30s of terminal alerts
 
     self._FACE_THRESHOLD = 0.7
     self._EYE_THRESHOLD = 0.65
